799999af9c73659b725c2eab7b5befb48ee9c199
patrizia-ducut <patriziaducut615@gmail.com>
2026-09-10 15:10:43 +0800

    debug: add prisma error event logging

--- PrintHub_Backend/db/prisma.js
+++ PrintHub_Backend/db/prisma.js
@@ -1,5 +1,16 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient, Prisma } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+console.log("Prisma engine version info:", JSON.stringify(Prisma.prismaVersion));
+
+const prisma = new PrismaClient({
+  log: [
+    { emit: "event", level: "error" },
+    { emit: "stdout", level: "warn" },
+  ],
+});
+
+prisma.$on("error", (e) => {
+  console.error("PRISMA RAW ERROR EVENT:", JSON.stringify(e));
+});
 
 module.exports = prisma;
